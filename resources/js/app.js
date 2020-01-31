378c59a9eff5f29ae68f47671fa06fc209f6d17e
Bogdan Popa <bogdan@defn.io>
2020-01-31 11:58:33 +0200

    ui: remember changes in localstorage

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,28 @@
 /* globals CodeMirror */
 (function() {
-  var editorEl = document.querySelector("#editor");
-  var formEl = document.querySelector("#form");
-  var evalEl = document.querySelector("#eval-button");
-  var editor = CodeMirror.fromTextArea(editorEl, {
-    lineNumbers: true,
-    mode: "scheme"
-  });
-
-  editor.setValue(`\
+  const source =
+    localStorage.getItem("source") ||
+    `\
 (define (fib n)
   (if (< n 2)
       n
       (+ (fib (- n 2))
          (fib (- n 1)))))
 
-(fib 8)`);
+(fib 8)`;
+
+  const editorEl = document.querySelector("#editor");
+  const formEl = document.querySelector("#form");
+  const evalEl = document.querySelector("#eval-button");
+  const editor = CodeMirror.fromTextArea(editorEl, {
+    lineNumbers: true,
+    mode: "scheme"
+  });
+
+  editor.setValue(source);
+  editor.on("change", function() {
+    localStorage.setItem("source", editorEl.value);
+  });
 
   evalEl.addEventListener("click", function() {
     formEl.submit();
